59694b284b4fb0b411cceeaa1f830bdb3d7d7418
Your Name <your.email@example.com>
2025-10-11 15:24:05 +0530

    new ui changes completed

--- src/app/_modules/admin/container/index.ts
+++ src/app/_modules/admin/container/index.ts
@@ -14,12 +14,12 @@ export const MENU: IMENU[] = [
         icon: 'assets/icons/user.png',
         isShow: true,
         subMenu: [
-            {
-                screen_name: 'User Create',
-                path: '/admin/user_create/create',
-                icon: 'assets/icons/add-user.png',
-                isShow: true,
-            },
+            // {
+            //     screen_name: 'User Create',
+            //     path: '/admin/user_create/create',
+            //     icon: 'assets/icons/add-user.png',
+            //     isShow: true,
+            // },
             {
                 screen_name: 'User List',
                 path: '/admin/user_create/list',
@@ -34,12 +34,12 @@ export const MENU: IMENU[] = [
         icon: 'assets/icons/project.png',
         isShow: true,
         subMenu: [
-            {
-                screen_name: 'Project Create',
-                path: '/admin/project_create/create',
-                icon: 'assets/icons/projectcreate.png',
-                isShow: true,
-            },
+            // {
+            //     screen_name: 'Project Create',
+            //     path: '/admin/project_create/create',
+            //     icon: 'assets/icons/projectcreate.png',
+            //     isShow: true,
+            // },
             {
                 screen_name: 'Project List',
                 path: '/admin/project_create/list',
@@ -54,12 +54,12 @@ export const MENU: IMENU[] = [
         icon: 'assets/icons/checked.png',
         isShow: true,
         subMenu: [
-            {
-                screen_name: 'Checklist Create',
-                path: '/admin/checklist_create/create',
-                icon: 'assets/icons/checklit-create.png',
-                isShow: true,
-            },
+            // {
+            //     screen_name: 'Checklist Create',
+            //     path: '/admin/checklist_create/create',
+            //     icon: 'assets/icons/checklit-create.png',
+            //     isShow: true,
+            // },
             {
                 screen_name: 'Checklist List',
                 path: '/admin/checklist_create/list',
@@ -88,12 +88,12 @@ export const MENU: IMENU[] = [
         icon: 'assets/icons/project.png',
         isShow: true,
         subMenu: [
-            {
-                screen_name: 'Role Mapping Create',
-                path: '/admin/userrolemapping/create',
-                icon: 'assets/icons/projectcreate.png',
-                isShow: true,
-            },
+            // {
+            //     screen_name: 'Role Mapping Create',
+            //     path: '/admin/userrolemapping/create',
+            //     icon: 'assets/icons/projectcreate.png',
+            //     isShow: true,
+            // },
             {
                 screen_name: 'Role Mapping List',
                 path: '/admin/userrolemapping/list',
